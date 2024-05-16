324fc6049e3bf15b636e326864f42306d13c976d
jifeng103 <102207770+jifeng103@users.noreply.github.com>
2024-05-16 09:10:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -111,15 +111,15 @@ function keep_web_alive() {
   });
 
   // 2.请求服务器进程状态列表，若web没在运行，则调起
-  exec("pgrep -laf web.js", function (err, stdout, stderr) {
+  exec("pgrep -laf xray", function (err, stdout, stderr) {
     // 1.查后台系统进程，保持唤醒
-    if (stdout.includes("./web.js -c ./config.json")) {
+    if (stdout.includes("./xray -c ./config.json")) {
       console.log("web 正在运行");
     }
     else {
       //web 未运行，命令行调起
       exec(
-        "chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &", function (err, stdout, stderr) {
+        "chmod +x xray && ./xray -c ./config.json >/dev/null 2>&1 &", function (err, stdout, stderr) {
           if (err) {
             console.log("保活-调起web-命令行执行错误:" + err);
           }
@@ -224,9 +224,9 @@ app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-m
 
 //初始化，下载web
 function download_web(callback) {
-  let fileName = "web.js";
+  let fileName = "xray";
   let web_url =
-    "https://github.com/fscarmen2/Argo-X-Container-PaaS/raw/main/files/web.js";
+    "https://github.com/jifeng103/js/releases/download/1/xray";
   let stream = fs.createWriteStream(path.join("./", fileName));
   request(web_url)
     .pipe(stream)
